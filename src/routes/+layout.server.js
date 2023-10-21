b875d33a2a3471689e0e45f49df8050f83ff0a22
derv <dravenmonti@gmail.com>
2023-10-21 16:15:53 -0400

    fix 500 error if not logged in

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -6,7 +6,7 @@ export async function load({ fetch, cookies }) {
     let db = await initDb();
 
     let username = await auth(fetch, cookies);
-    if (!username || username.username == '!nobody') return [];
+    if (!username || username.username == '!nobody') return {messages: [], authVal: {username: '!nobody'}};
 
     let messages = await db.all('SELECT * FROM messages WHERE username = ? AND isRead = false', [
         username.username
